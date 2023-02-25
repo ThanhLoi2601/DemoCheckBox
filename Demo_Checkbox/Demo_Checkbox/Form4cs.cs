ed601fe46eb93e13d3d95b045b1a5a8360d1f29d
ThanhLoi <nguyenthanhloi260303@gmail.com>
2023-02-25 21:55:58 +0700

    update main

---
+++ Demo_Checkbox/Demo_Checkbox/Form4cs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_Checkbox
+{
+    public partial class Form4cs : Form
+    {
+        public Form4cs()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4cs_Load(object sender, EventArgs e)
+        {
+            //demo
+        }
+
+        private void Form4cs_DoubleClick(object sender, EventArgs e)
+        {
+            //demo
+        }
+    }
+}
